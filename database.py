99d6f911b68bff76d916629acaf05ff407fc23c5
JulioCSSousa <julio.sousa1993@gmail.com>
2023-08-14 21:40:20 -0300

    sincronizado com railway

--- database.py
+++ database.py
@@ -4,16 +4,16 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import marshmallow
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:7hTknayAdiMlWMbuVzgI@containers-us-west-144.railway.app:6009/railway'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:2ZXdXJSnwlwVKk7553mP@containers-us-west-49.railway.app:6614/railway'
 app.config['SECRET_KEY'] = 'secret'
 
 login_manager = LoginManager(app)
 db = SQLAlchemy(app)
 ma = marshmallow
 mydb = mysql.connector.connect(
-    host='containers-us-west-144.railway.app',
+    host='containers-us-west-49.railway.app',
     user='root',
-    password= '7hTknayAdiMlWMbuVzgI',
-    database='railway')
+    password='2ZXdXJSnwlwVKk7553mP',
+    port='6614')
 
 cursor = mydb.cursor()
