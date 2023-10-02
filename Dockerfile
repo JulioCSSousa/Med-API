8045d344f00a3eda3265d952acabd1167492f2b3
JulioCSSousa <julio.sousa1993@gmail.com>
2023-10-02 20:53:24 -0300

    end points prontos, teste de servidor

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,6 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 COPY . app/code/main.py
 
 #
-CMD ["uvicorn", "app.main:app"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
 
 #, "--host", "0.0.0.0", "--port", "8000
--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URI = 'mysql://root:159753@localhost:3306/pymeddb'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:ZTaQxeQttlYLn77wJ6sb@containers-us-west-50.railway.app:6233/railway'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 
